69a31d249f235bca2b5781a7b1c57fe31d909a0d
0xFreDi <62937543+oveeauki@users.noreply.github.com>
2024-07-19 19:10:55 +0300

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,18 +1,17 @@
 #include "print.h"
 
 void kernel_main(){
-for(int i=0;i<10;i++){
-print(0x26, "Hello World!\0");
-}
+ const char *stra = {"hello\0"};
+  print(PRINT_COLOR_BLUE,stra);
 }
 
-void print(int color, const char *str){
+void print(int color,const char *str){
 int pos = 0;
 char *BASE = 0;
-BASE = (char*)0xb8000;
-    while(*str != '\0'){
-        BASE[pos--] = *str;
-        str++;
-        BASE[pos--] = color;
-   }
+BASE = (char*)0xb8000; 
+  while(*str != '\0') {
+          BASE[pos++] = *str; 
+          BASE[pos++] = color; 
+          str++;
+      }
 }
